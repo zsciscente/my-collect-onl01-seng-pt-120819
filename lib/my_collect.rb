b51452a6575408e4d58b0d2d97054e24c45b2095
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 14:14:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
+def my_collect
+  students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+  language = ['ruby', 'javascript', 'python', 'objective-c']
+  
+counter = 0
+  while language.length > counter 
+  yield(language) 
+  { |i| i.upcase }
+  
+  counter += 1
+end
+language
 
 
+counter2 = 0
+  while students.length > counter2 
+  yield(students) do |name| 
+    name.split(" ").first 
+  end
+  
+  counter2 += 1
+end
+ students
+
+
+
+
+
+
+end
